ff3571316e18b7b1479db4d32a203c4cbff8fa38
Ivan Ortega Alba <ivanortegaalba@MacBook-Pro-de-Ivan.local>
2015-03-31 22:44:06 +0200

    Intentando cambiar las posiciones del slider

--- js/slider.js
+++ js/slider.js
@@ -1,81 +1,50 @@
 var ul;
-var liItems;
-var imageWidth;
-var imageNumber;
-var currentImage = 0;
+var li_items;
+var li_number;
+var slider_width = 0;
+var current = 0;
+var positions = [0];
+var actual_page_size = 0;
+var actual_procesed_size = 0;
+var actual_position = 0;
 
-//Cambiamos el tamaño del ul al total de todas las imagenes alineadas
 function init() {
-
     ul = document.getElementById('image_slider');
-    liItems = ul.children;
-    imageNumber = liItems.length;
-    imageWidth = liItems[0].children[0].offsetWidth;
-    ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
-    slider(ul);
-//    Prueba para hacer el tamaño del slider dinámico(controlar por css que no rompa la estuctura)
-//    var computedStyle = document.defaultView.getComputedStyle(liItems[i].childNodes[0], null);
-//    imageWidth = computedStyle.width;
+    li_items = ul.children;
+    li_number = li_items.length;
+    slider_width = document.getElementById('patrocinadores').clientWidth;
+
+    for (i = 0; i < li_number; i++) {
+        image_width = li_items[i].childNodes[1].clientWidth;
+        if (image_width + actual_page_size < slider_width)
+            actual_page_size += image_width;
+        else {
+            positions.push(actual_procesed_size);
+            actual_page_size = 0;
+        }
+        actual_procesed_size += image_width;
+    }
+
+    ul.style.width = parseInt(slider_width) + 'px';
+    setTimeout(slider(), 2000);
 }
 
-/**delta function is to set how the image slide—keep still for a while and move to next picture.
-*step function will be called many times until clearInterval() been called
-* currentImage * imageWidth is the currentImage position of ul
-* delta start from 0 to 1, delta * imageWidth is the pixels that changes
-**/
- function slider(ul){
-    animate({
-        delay:17,
-        duration: 3000,
-        delta:function(p){return Math.max(0, -1 + 2 * p)},
-        step:function(delta){
-            ul.style.left = '-' + parseInt(currentImage * imageWidth + delta * imageWidth) + 'px';
-    },
-        callback:function(){
-            currentImage++;
-        // if it doesn't slied to the last image, keep sliding
-            if(currentImage < imageNumber-1){
-                slider(ul);
-        }
-       // if current image it's the last one, it slides back to the first one
-            else{
-                var leftPosition = (imageNumber - 1) * imageWidth;
-               // after 2 seconds, call the goBack function to slide to the first image
-                setTimeout(function(){goBack(leftPosition)},2000);
-                setTimeout(function(){slider(ul)}, 4000);
-            }
-        }
-    });
+function slider() {
+
+    if (actual_position < positions.length) {
+        ul.style.left = "-" + positions[actual_position] + "px";
+        actual_position++;
+        setTimeout(slider(), 2000);
+    } else {
+        // after 2 seconds, call the goBack function to slide to the first image
+        setTimeout(function() {
+            ul.style.left = 0 + "px";
+            actual_position = 0;
+        }, 2000);
+        setTimeout(function() {
+            slider()
+        }, 4000);
+    }
 }
 
-function goBack(leftPosition){
-    currentImage = 0;
-    var id = setInterval(function(){
-        if(leftPosition >= 0){
-            ul.style.left = '-' + parseInt(leftPosition) + 'px';
-            leftPosition -= imageWidth / 10;
-        }
-        else{
-            clearInterval(id);
-        }
-    }, 17);
-}
-
-//generic animate function
-function animate(opts){
-    var start = new Date;
-    var id = setInterval(function(){
-        var timePassed = new Date - start;
-        var progress = timePassed / opts.duration
-        if(progress > 1){
-            progress = 1;
-        }
-        var delta = opts.delta(progress);
-        opts.step(delta);
-        if (progress == 1){
-            clearInterval(id);
-           opts.callback();
-         }
-    }, opts.dalay || 17);
-}
 window.onload = init;
