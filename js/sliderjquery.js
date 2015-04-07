cb611713c2f971f3b4c9fd4ddaa23d30e96dd145
Ivan Ortega Alba <ivanortegaalba@MacBook-Pro-de-Ivan.local>
2015-04-07 18:00:26 +0200

    Con animaciones CSS

--- js/sliderjquery.js
+++ js/sliderjquery.js
@@ -20,46 +20,47 @@
 //        });
 //}
 
-var imagenes;
-var tamano;
+//var imagenes;
+//var tamano;
+//var i = 0;
+//var animacion = [];
+//
+//function movimientoIzq(idImagen) {
+//    marginleft = imagenes[idImagen].style.marginLeft
+//    marginleft.replace("px", "");
+//    marginleft = parseInt(marginleft);
+//    marginleft++;
+//    imagenes[idImagen].style.marginLeft = (marginleft.toString().concat("px"));
+//    console.log(imagenes[idImagen])
+//
+//}
+//
+//
+//function slider(imagenes) {
+//
+//    if (i < imagenes.length) {
+//        imagenes[i].style.zIndex = "10" + i;
+//        imagenes[i].style.marginLeft = "0px";
+//        idAnima = setInterval("movimientoIzq(i)", 10);
+//        animacion[0] = idAnima
+//        i += 2;
+//    }
+//    //    else
+//    //        for (img in imagenes)
+//    //            img.style.marginLeft = 0;
+//}
+//
+//function iniciarSlider(div) {
+//    imagenes = div.childNodes;
+//    tamano = div.style.width;
+//    i = 1;
+//    for (img = 1; img < imagenes.length; img += 2)
+//        imagenes[img].style.marginLeft = "0px";
+//    setInterval("slider(imagenes)", 6000);
+//}
+
+var imagenes = document.getElementById("slideshow").getElementsByTagName("img");
 var i = 0;
-var animacion = [];
-
-function movimientoIzq(idImagen) {
-    marginleft = imagenes[idImagen].style.marginLeft
-    marginleft.replace("px", "");
-    marginleft = parseInt(marginleft);
-    marginleft++;
-    imagenes[idImagen].style.marginLeft = (marginleft.toString().concat("px"));
-    console.log(imagenes[idImagen])
-
-}
-
-
-function slider(imagenes) {
-
-    if (i < imagenes.length) {
-        imagenes[i].style.zIndex = "10" + i;
-        imagenes[i].style.marginLeft = "0px";
-        idAnima = setInterval("movimientoIzq(i)", 10);
-        animacion[0] = idAnima
-        i += 2;
-    }
-    //    else
-    //        for (img in imagenes)
-    //            img.style.marginLeft = 0;
-}
-
-function iniciarSlider(div) {
-    imagenes = div.childNodes;
-    tamano = div.style.width;
-    i = 1;
-    for (img = 1; img < imagenes.length; img += 2)
-        imagenes[img].style.marginLeft = "0px";
-    setInterval("slider(imagenes)", 6000);
-}
-
-
 function cambiarClaseActiva(imagenes) {
     imagenes[i].classList.add('active');
     if (i < imagenes.length) {
@@ -68,7 +69,7 @@ function cambiarClaseActiva(imagenes) {
     } else
         setTimeout(cambiarClaseActiva(imagenes));
 }
-var imagenes = document.getElementById("slideshow").getElementsByTagName("img");
+
 setInterval("cambiarClaseActiva(imagenes)", 6000);
 
 window.onload = cambiarClaseActiva(imagenes);
