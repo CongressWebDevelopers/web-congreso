1d302dfeeebe13bc614710c68396fb6dbe1d5491
ivanortegaalba <ivanortegaalba@gmail.com>
2015-04-10 16:37:05 +0200

    Añadido nuevo estilo de validación y no envia form

--- js/formulario.js
+++ js/formulario.js
@@ -1,9 +1,21 @@
-function validar() {
-    var correo= document.getElementById('email');
-    
-    if (/^\w+([\.\-\_]?\w+)*@\w+([\.-]?\w+)*(\.\D{2,4})+$/.test(correo.value)){ 
-        alert("!Gracias por contactar con nosotros! Le atenderemos con la mayor brevedad posible.");    
-    }else{
-        alert("La dirección de email " + correo.value + " es incorrecta. Reinténtelo");
+function validar(evObject) {
+    var correo = document.getElementById('email');
+    var error = document.getElementById("contacto-notification")
+
+    if (/^\w+([\.\-\_]?\w+)*@\w+([\.-]?\w+)*(\.\D{2,4})+$/.test(correo.value)) {
+        if (error.classList.contains("error"))
+            error.classList.remove("error")
+        error.classList.add("success");
+        error.innerText = "!Gracias por contactar con nosotros! Le atenderemos con la mayor brevedad posible.";
+        error.style.display = 'block';
+
+    } else {
+        error.innerText = "La dirección de email " + correo.value + " es incorrecta. Reinténtelo";
+        if (error.classList.contains("success"))
+            error.classList.remove("success")
+        error.classList.add("error");
+        error.style.display = 'block';
+
     }
+    return false;
 }
