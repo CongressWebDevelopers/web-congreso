856877060895d39fe2bc151e3f92af3099d76c65
ivanortegaalba <ivanortegaalba@gmail.com>
2015-04-22 10:43:03 +0200

    Mejora para evitar notice

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 require_once('head.php');
 require_once('header.php');
-if($_GET['page']=='programa')
-    require_once('./pages/programa.php');
+
+if(isset($_GET['page']))
+    switch ($_GET['page']) {
+        case "programa":
+            require_once('./pages/programa.php');
+            break;
+        //Añadir el resto de opciones del menu
+        default:
+            require_once('contain.php');
+    }
 else
     require_once('contain.php');
+
 require_once('footer.php');
 ?>
