78e9810db0cb067c7a3eb40f56407b32a8c52b89
ivanortegaalba <ivanortegaalba@gmail.com>
2015-05-09 00:41:26 +0200

    Arregaldo email

--- modulos/mail/mail.php
+++ modulos/mail/mail.php
@@ -3,29 +3,35 @@
  * Modulo que manda dos correos al recibir la petición POST del formulario de contacto.
  */
 if (isset($_POST['enviar'])) {
+    //Datos del interesado
+    $nombreInteresado = $_POST['nombre'];
+    $emailInteresado = $_POST['email'];
+    $asuntoInteresado = $_POST['asunto'];
+    $cuestionInteresado = $_POST['cuestion'];
+    
     //Cuenta asociada a la persona de contacto del CEIIE
-    $DESTINATARIO = 'ivanortegaalba@me.com';
+    $HOST = 'ivanortegaalba@gmail.com';
 
     //MENSAJE PARA EL CEII
-    $para = $DESTINATARIO;
-    $titulo = "[Mensaje de Web] " . $_POST['asunto'];
-    $mensaje = "Nombre: " . $_POST['nombre'] . "\n\n" .
-            "Email: " . $_POST['email'] . "\n\n" .
+    $para = $HOST;
+    $titulo = "[Mensaje de Web] " . $asuntoInteresado;
+    $mensaje = "Nombre: " . $nombreInteresado . "\n\n" .
+            "Email: " . $emailInteresado . "\n\n" .
             $_POST['cuestion'];
-    $cabeceras = 'From: ' . $_POST['nombre'] . '<' . $_POST['email'] . ">\r\n" .
-            'Reply-To:' . $_POST['nombre'] . '<' . $_POST['email'] . ">\r\n" .
+    $cabeceras = 'From: Web CEIIE<' . $para . ">\r\n" .
+            'Reply-To:' . $nombreInteresado . '<' . $emailInteresado . ">\r\n" .
             'X-Mailer: PHP/' . phpversion();
     //Envio a la organización
     $enviadoHost = mail($para, $titulo, $mensaje, $cabeceras);
 
     //COPIA PARA EL INTERESADO
-    $para = $_POST['email'];
-    $titulo = "[Copia de Mensaje de Web] " . $_POST['asunto'];
-    $mensaje = "Nombre: " . $_POST['nombre'] . "\n\n" .
-            "Email: " . $_POST['email'] . "\n\n" .
+    $para = $emailInteresado;
+    $titulo = "[Copia de Mensaje de Web] " . $asuntoInteresado;
+    $mensaje = "Nombre: " . $nombreInteresado . "\n\n" .
+            "Email: " . $emailInteresado . "\n\n" .
             $_POST['cuestion'] . "\n\n Gracias por contactar, le responderemos tan pronto como sea posible";
-    $cabeceras = 'From: CEIIE<' . $DESTINATARIO . ">\r\n" .
-            'Reply-To:CEIIE<' . $DESTINATARIO . ">\r\n" .
+    $cabeceras = 'From: CEIIE<' . $HOST . ">\r\n" .
+            'Reply-To:CEIIE<' . $HOST . ">\r\n" .
             'X-Mailer: PHP/' . phpversion();
     //Envio de la copia al interesado
     $enviadoCliente = mail($para, $titulo, $mensaje, $cabeceras);
