4a96df799f75c737a0a61ad097552c70f9c80970
ivanortegaalba <ivanortegaalba@gmail.com>
2015-05-18 01:39:27 +0200

    Creada la clase Contenedor congresista #56

--- php/model/containers/contenedorCongresista.php
+++ php/model/containers/contenedorCongresista.php
@@ -1,16 +1,32 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 include_once 'php/database/ORM.php';
-class ContenedorCongresista{
-    function ContenedorCongresista(){
-        $orm = new ORM();
+
+class ContenedorCongresista {
+
+    private $orm;
+
+    function __construct() {
+        $this->$orm = new ORM();
     }
-    function getCongresistaID($id){
-        $orm->
+
+    function getCongresistaID($id) {
+        $resultRow = $orm->queryArray("SELECT * FROM congresista WHERE idCongresista = " . $id);
+        $congresista = new Congresista($resultRow);
+        return $congresista;
     }
+
+    function getAll() {
+        $congresistas = array();
+        $resultRow = $this->$orm->queryArray("SELECT * FROM congresista");
+        foreach ($resultRow as $congresista) {
+            $congresistas[] = new Congresista($congresista);
+        }
+    }
+
 }
