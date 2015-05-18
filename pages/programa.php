0166ee36b9c3cc24e256b06f29959af5ef9da8bd
ivanortegaalba <ivanortegaalba@gmail.com>
2015-05-18 15:46:01 +0200

    prueba getBreadCump() #63

--- pages/programa.php
+++ pages/programa.php
@@ -1,18 +1,5 @@
-<div class="wrapper col3">
-    <div id="breadcrumb">
-        <ul>
-            <li class="first">Tú estás aquí</li>
-            <li>&#187;</li>
-            <li><a href="#">Inicio</a>
-            </li>
-            <li>&#187;</li>
-            <li class="current"><a href="#">Programa</a>
-            </li>
-        </ul>
-    </div>
-</div>
-<div class="wrapper col5">
-    <div id="container">
+    <?php getBreadCrumb("Programa")?>
+
         <table border="1">
             <thead>
             <th>Hora</th>
@@ -108,6 +95,5 @@
                 $(this).children("div").toggle("slow");
             });
         </script>
-    </div>
-</div>
+
 
