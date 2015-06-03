033cd555e3242a961bfb1d11ace19a1447a15146
ivanortegaalba <ivanortegaalba@gmail.com>
2015-06-03 18:21:26 +0200

    Actualizada db

--- database-scripts/db_CEIIE.sql
+++ database-scripts/db_CEIIE.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 02-06-2015 a las 19:03:33
+-- Tiempo de generación: 03-06-2015 a las 18:20:39
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -34,6 +34,25 @@ CREATE TABLE IF NOT EXISTS `actividad` (
   `hora` time NOT NULL,
   `importe` int(11) NOT NULL,
   `foto` varchar(250) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `actividad`
+--
+
+INSERT INTO `actividad` (`idActividad`, `denominacion`, `descripcion`, `fecha`, `hora`, `importe`, `foto`) VALUES
+(33, 'Visita a la Alhambra', 'La Alhambra representa todo el esplendor y el poder de la dinastÃ­a nazarÃ­. Su toma se produjo el 2 de enero de 1492 con la entrada de los Reyes CatÃ³licos en la ciudad tras una guerra de 10 aÃ±os. El primer sultÃ¡n de la Alhambra fue Muhammad I o Al-Ahmar, el fundador de la dinastÃ­a nazarÃ­, y el Ãºltimo sultÃ¡n: Muhammad XII, mÃ¡s conocido como Boabdil.\r\n\r\nLa Alhambra de Granada es el monumento mÃ¡s visitado de EspaÃ±a. 3 millones de turistas visitaron la Alhambra y el Generalife en 2012. Este conjunto monumental fue declarado Patrimonio de la Humanidad por la Unesco en el aÃ±o 1984.\r\n\r\nDesde este 1Âº Congreso de IngenierÃ­a InformÃ¡tica, no queremos dejar pasar la oportunidad de hacer un tour guiado por la Alhambra y los jardines del Generalife.\r\n', '2016-01-30', '20:00:00', 10, 'alhambr.jpg'),
+(34, 'Sierra Nevada', 'Enclavada en la parte central de la Cordillera PenibÃ©tica, el macizo de Sierra Nevada constituye la mÃ¡s extensa de las Ã¡reas montaÃ±osas ibÃ©ricas dotadas de unidad estructural propia. Denominada por los Ã¡rabes de la Edad Media como â€œSierra del Solâ€, alberga no sÃ³lo una de las floras mas valiosas de todo el continente europeo, sino que ademÃ¡s esconde un rico patrimonio cultural e histÃ³rico acumulado durante siglos.\r\n\r\nLa estaciÃ³n de esquÃ­ se encuentra en pleno corazÃ³n del Parque Nacional de Sierra Nevada, siendo Ã©sta la mÃ¡s alta de EspaÃ±a y la mÃ¡s merdional de Europa. Estas condiciones hacen que la estaciÃ³n de esquÃ­ de Sierra Nevada , sea la que tenga mÃ¡s horas de Sol de toda Europa; convirtiÃ©ndola tambiÃ©n en la mÃ¡s visitada de EspaÃ±a. Es mÃ¡s, Sierra Nevada estÃ¡ considerada la estaciÃ³n de esquÃ­ con mÃ¡s ambiente nocturno de Europa.', '2016-01-02', '07:00:00', 15, 'esqui.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividadcuota`
+--
+
+CREATE TABLE IF NOT EXISTS `actividadcuota` (
+  `idCuota` int(11) DEFAULT NULL,
+  `idActividad` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -47,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `cuota` (
   `denominacion` varchar(200) NOT NULL,
   `descripcion` text NOT NULL,
   `importe` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `cuota`
@@ -55,7 +74,18 @@ CREATE TABLE IF NOT EXISTS `cuota` (
 
 INSERT INTO `cuota` (`idCuota`, `denominacion`, `descripcion`, `importe`) VALUES
 (1, 'Cuota 1', 'lalalalala', 20),
-(2, 'Cuota 2', 'lelelele', 30);
+(2, 'Cuota 2', 'lelelele', 30),
+(3, 'Profesor', 'El ir al congreso en calidad de profesor, implica que eres docente de alguno de los centros de estudios superiores de IngenierÃ­a InformÃ¡tica.', 50),
+(4, 'Alumno', 'Ser alumno es lo que tiene', 50),
+(5, 'Alumno', 'Ser alumno es lo que tiene', 50),
+(6, 'Alumno', 'Ser alumno es lo que tiene', 50),
+(7, 'Alumno', 'Ser alumno es lo que tiene', 50),
+(8, 'Alumno', 'Ser alumno es lo que tiene', 50),
+(9, 'Alumno', 'Ser alumno es lo que tiene', 50),
+(10, 'Alumno', 'Ser alumno es lo que tiene', 50),
+(11, 'Alumno', 'Ser alumno es lo que tiene', 50),
+(12, 'Alumno', 'Ser alumno es lo que tiene', 50),
+(13, 'Alumno', 'Ser alumno es lo que tiene', 50);
 
 -- --------------------------------------------------------
 
@@ -110,6 +140,12 @@ ALTER TABLE `actividad`
  ADD PRIMARY KEY (`idActividad`);
 
 --
+-- Indices de la tabla `actividadcuota`
+--
+ALTER TABLE `actividadcuota`
+ ADD KEY `idActividad_idx` (`idActividad`), ADD KEY `idCuota_idx` (`idCuota`);
+
+--
 -- Indices de la tabla `cuota`
 --
 ALTER TABLE `cuota`
@@ -135,12 +171,12 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `actividad`
 --
 ALTER TABLE `actividad`
-MODIFY `idActividad` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `idActividad` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
 --
 -- AUTO_INCREMENT de la tabla `cuota`
 --
 ALTER TABLE `cuota`
-MODIFY `idCuota` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `idCuota` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT de la tabla `Inscripcion`
 --
@@ -151,6 +187,17 @@ MODIFY `idInscripcion` int(11) NOT NULL AUTO_INCREMENT;
 --
 ALTER TABLE `usuario`
 MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `actividadcuota`
+--
+ALTER TABLE `actividadcuota`
+ADD CONSTRAINT `idActividad` FOREIGN KEY (`idActividad`) REFERENCES `actividad` (`idActividad`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `idCuota` FOREIGN KEY (`idCuota`) REFERENCES `cuota` (`idCuota`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
