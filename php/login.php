6754641a5e7cbf1bd8507915bdcdf21ee0b2e47b
ivanortegaalba <ivanortegaalba@gmail.com>
2015-06-04 12:01:08 +0200

    Quitado el notice de inicio de sesión

--- php/login.php
+++ php/login.php
@@ -4,14 +4,16 @@ include_once 'php/model/containers/ContenedorUsuario.php';
 include_once 'php/model/containers/ContenedorInscripcion.php';
 
 if (!isset($_SESSION['usuario'])) {
-    session_start();
+    if (session_id() == "")
+        session_start();
 }
 if (isset($_POST['login'])) {
     $usuarioLogin = $_POST['usuario'];
     $passwordLogin = $_POST['password'];
     $cUsuario = new ContenedorUsuario();
     if ($usuarioActual = $cUsuario->comprobarLogin($usuarioLogin, $passwordLogin)) {
-        session_start();
+        if (session_id() == "")
+            session_start();
         $cInscripcion = new ContenedorInscripcion();
         $_SESSION['usuario'] = $usuarioActual;
         $_SESSION['inscrito'] = $cInscripcion->estaInscrito($usuarioActual->getId());
