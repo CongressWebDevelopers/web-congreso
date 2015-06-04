fc10acd159fc2157a3c9b90ecdf8df5be2117ece
jota-ele256 <jota_ele256@hotmail.com>
2015-06-04 11:10:51 +0200

    añadido api, sin completar

---
+++ rest/configDB.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define('DB_HOST', 'localhost');
+
+define('DB_USER', 'admin-congreso');
+
+define('DB_PASS', 'congreso');
+
+define('DB_NAME', 'db_hoteles');
+
+?>
+
