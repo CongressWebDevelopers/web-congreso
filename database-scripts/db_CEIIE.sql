697fcb893cfe94a8d2e36f5e63278f3f7a9b4ea5
ivanortegaalba <ivanortegaalba@gmail.com>
2015-06-18 18:33:44 +0200

    Actualizada BD

--- database-scripts/db_CEIIE.sql
+++ database-scripts/db_CEIIE.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 03-06-2015 a las 19:27:55
+-- Tiempo de generación: 18-06-2015 a las 18:32:02
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -34,15 +34,16 @@ CREATE TABLE IF NOT EXISTS `actividad` (
   `hora` time NOT NULL,
   `importe` int(11) NOT NULL,
   `foto` varchar(250) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `actividad`
 --
 
 INSERT INTO `actividad` (`idActividad`, `denominacion`, `descripcion`, `fecha`, `hora`, `importe`, `foto`) VALUES
-(33, 'Visita a la Alhambra', 'La Alhambra representa todo el esplendor y el poder de la dinastÃ­a nazarÃ­. Su toma se produjo el 2 de enero de 1492 con la entrada de los Reyes CatÃ³licos en la ciudad tras una guerra de 10 aÃ±os. El primer sultÃ¡n de la Alhambra fue Muhammad I o Al-Ahmar, el fundador de la dinastÃ­a nazarÃ­, y el Ãºltimo sultÃ¡n: Muhammad XII, mÃ¡s conocido como Boabdil.\r\n\r\nLa Alhambra de Granada es el monumento mÃ¡s visitado de EspaÃ±a. 3 millones de turistas visitaron la Alhambra y el Generalife en 2012. Este conjunto monumental fue declarado Patrimonio de la Humanidad por la Unesco en el aÃ±o 1984.\r\n\r\nDesde este 1Âº Congreso de IngenierÃ­a InformÃ¡tica, no queremos dejar pasar la oportunidad de hacer un tour guiado por la Alhambra y los jardines del Generalife.\r\n', '2016-01-30', '20:00:00', 10, 'alhambr.jpg'),
-(34, 'Sierra Nevada', 'Enclavada en la parte central de la Cordillera PenibÃ©tica, el macizo de Sierra Nevada constituye la mÃ¡s extensa de las Ã¡reas montaÃ±osas ibÃ©ricas dotadas de unidad estructural propia. Denominada por los Ã¡rabes de la Edad Media como â€œSierra del Solâ€, alberga no sÃ³lo una de las floras mas valiosas de todo el continente europeo, sino que ademÃ¡s esconde un rico patrimonio cultural e histÃ³rico acumulado durante siglos.\r\n\r\nLa estaciÃ³n de esquÃ­ se encuentra en pleno corazÃ³n del Parque Nacional de Sierra Nevada, siendo Ã©sta la mÃ¡s alta de EspaÃ±a y la mÃ¡s merdional de Europa. Estas condiciones hacen que la estaciÃ³n de esquÃ­ de Sierra Nevada , sea la que tenga mÃ¡s horas de Sol de toda Europa; convirtiÃ©ndola tambiÃ©n en la mÃ¡s visitada de EspaÃ±a. Es mÃ¡s, Sierra Nevada estÃ¡ considerada la estaciÃ³n de esquÃ­ con mÃ¡s ambiente nocturno de Europa.', '2016-01-02', '07:00:00', 15, 'esqui.jpg');
+(33, 'Visita a la Alhambra', 'La Alhambra representa todo el esplendor y el poder de la dinastÃ­a nazarÃ­. Su toma se produjo el 2 de enero de 1492 con la entrada de los Reyes CatÃ³licos en la ciudad tras una guerra de 10 aÃ±os. El primer sultÃ¡n de la Alhambra fue Muhammad I o Al-Ahmar, el fundador de la dinastÃ­a nazarÃ­, y el Ãºltimo sultÃ¡n: Muhammad XII, mÃ¡s conocido como Boabdil.\r\n\r\nLa Alhambra de Granada es el monumento mÃ¡s visitado de EspaÃ±a. 3 millones de turistas visitaron la Alhambra y el Generalife en 2012. Este conjunto monumental fue declarado Patrimonio de la Humanidad por la Unesco en el aÃ±o 1984.\r\n\r\nDesde este 1Âº Congreso de IngenierÃ­a InformÃ¡tica, no queremos dejar pasar la oportunidad de hacer un tour guiado por la Alhambra y los jardines del Generalife.', '2016-01-02', '07:00:00', 15, 'alhambr.jpg'),
+(34, 'Sierra Nevada', 'Enclavada en la parte central de la Cordillera PenibÃ©tica, el macizo de Sierra Nevada constituye la mÃ¡s extensa de las Ã¡reas montaÃ±osas ibÃ©ricas dotadas de unidad estructural propia. Denominada por los Ã¡rabes de la Edad Media como â€œSierra del Solâ€, alberga no sÃ³lo una de las floras mas valiosas de todo el continente europeo, sino que ademÃ¡s esconde un rico patrimonio cultural e histÃ³rico acumulado durante siglos.\r\n\r\nLa estaciÃ³n de esquÃ­ se encuentra en pleno corazÃ³n del Parque Nacional de Sierra Nevada, siendo Ã©sta la mÃ¡s alta de EspaÃ±a y la mÃ¡s merdional de Europa. Estas condiciones hacen que la estaciÃ³n de esquÃ­ de Sierra Nevada , sea la que tenga mÃ¡s horas de Sol de toda Europa; convirtiÃ©ndola tambiÃ©n en la mÃ¡s visitada de EspaÃ±a. Es mÃ¡s, Sierra Nevada estÃ¡ considerada la estaciÃ³n de esquÃ­ con mÃ¡s ambiente nocturno de Europa.', '2016-01-02', '07:00:00', 15, 'esqui.jpg'),
+(35, 'Visita a los Cahorros', 'Monachil es un pueblo que estÃ¡ situado a tan sÃ³lo unos 8 kilÃ³metros hacia el sureste de Granada, en la parte del centro-sur de la comarca de la Vega de Granada. Es aquÃ­ donde estÃ¡n Los Cahorros, una zona de alucinantes paisajes ideal para hacer senderismo o practicar escalada.', '2016-01-02', '07:00:00', 15, 'cahorros.jpg');
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,14 @@ INSERT INTO `actividadcuota` (`idActividad`, `idCuota`) VALUES
 (33, 17),
 (33, 18),
 (33, 19),
-(34, 19);
+(34, 19),
+(33, 20),
+(34, 20),
+(35, 20),
+(33, 15),
+(33, 21),
+(35, 21),
+(34, 16);
 
 -- --------------------------------------------------------
 
@@ -77,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `cuota` (
   `denominacion` varchar(200) NOT NULL,
   `descripcion` text NOT NULL,
   `importe` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `cuota`
@@ -88,7 +96,9 @@ INSERT INTO `cuota` (`idCuota`, `denominacion`, `descripcion`, `importe`) VALUES
 (16, 'Profesor', 'Profesorado', 50),
 (17, 'Profesor', 'ssss', 50),
 (18, 'Profesor', 'ssss', 50),
-(19, 'Visita a la Alhambra', 'sss', 20);
+(19, 'Visita a la Alhambra', 'sss', 21),
+(20, 'Todo incluido', 'Esta cuota incluye todo', 100),
+(21, 'Exponente', 'Esta cuota incluye a los exponentes en alguna temÃ¡tica o SPRINT del congreso.', 40);
 
 -- --------------------------------------------------------
 
@@ -106,7 +116,54 @@ CREATE TABLE IF NOT EXISTS `Inscripcion` (
   `fechaSalida` date NOT NULL,
   `fechaEntrada` date NOT NULL,
   `idUsuario` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `Inscripcion`
+--
+
+INSERT INTO `Inscripcion` (`idInscripcion`, `nombre`, `centro`, `telefono`, `cuota`, `hotel`, `fechaSalida`, `fechaEntrada`, `idUsuario`) VALUES
+(2, 'Ivan Ortega Alba 2', 'UGR', '680', 20, 0, '0000-00-00', '0000-00-00', 11),
+(3, 'Ivan Ortega Alba 3', 'UGR', '680', 17, 0, '0000-00-00', '0000-00-00', 12),
+(5, 'Francisco', 'UGR', '680178921', 19, 0, '0000-00-00', '0000-00-00', 13),
+(6, 'German', 'UGR', '680446758', 20, 0, '0000-00-00', '0000-00-00', 14),
+(7, 'yoyoyo', 'jajaj', '12312314', 20, 0, '0000-00-00', '0000-00-00', 15);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inscripcionactividad`
+--
+
+CREATE TABLE IF NOT EXISTS `inscripcionactividad` (
+  `idInscripcion` int(11) NOT NULL,
+  `idActividad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `inscripcionactividad`
+--
+
+INSERT INTO `inscripcionactividad` (`idInscripcion`, `idActividad`) VALUES
+(1, 33),
+(1, 34),
+(1, 35),
+(2, 33),
+(2, 34),
+(2, 35),
+(3, 33),
+(4, 33),
+(4, 34),
+(4, 35),
+(5, 33),
+(5, 34),
+(5, 35),
+(6, 33),
+(6, 34),
+(6, 35),
+(7, 33),
+(7, 34),
+(7, 35);
 
 -- --------------------------------------------------------
 
@@ -119,18 +176,21 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `nombreUsuario` varchar(100) NOT NULL,
   `password` varchar(250) NOT NULL,
   `rol` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
 INSERT INTO `usuario` (`idUsuario`, `nombreUsuario`, `password`, `rol`) VALUES
-(1, 'ivanortegaalba@gmail.com', '', 2),
-(2, 'ivanortegaalba@gmail.com', '', 2),
-(3, 'ivanortegaalba@gmail.com', 'd41d8cd98f00b204e9800998ecf8427e', 2),
-(4, 'ivanortegaalba@gmail.com', '0861ffa03c95ef4aa7c1fd2327dab91d', 2),
-(5, 'ivanortegaalba@gmail.com', '', 2);
+(4, 'ivanortegaalba@gmail.com', '2d01b437ef5276d5fef064e19800ebee', 0),
+(10, 'ivanortegaalba@hotmail.com', '0861ffa03c95ef4aa7c1fd2327dab91d', 0),
+(11, '1234', '1234', 1),
+(12, 'ivan', '2c42e5cf1cdbafea04ed267018ef1511', 1),
+(13, 'fjmelero@ugr.es', '944bc406c52b4f411be922897b0936e2', 1),
+(14, 'usuariodeprueba@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 1),
+(15, 'fjmelero@ugr.es', '944bc406c52b4f411be922897b0936e2', 1),
+(16, 'jota_ele256@hotmail.com', 'e26e64e9987829d9801ac9845b08593d', 1);
 
 --
 -- Índices para tablas volcadas
@@ -168,22 +228,22 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `actividad`
 --
 ALTER TABLE `actividad`
-MODIFY `idActividad` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
+MODIFY `idActividad` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT de la tabla `cuota`
 --
 ALTER TABLE `cuota`
-MODIFY `idCuota` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+MODIFY `idCuota` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT de la tabla `Inscripcion`
 --
 ALTER TABLE `Inscripcion`
-MODIFY `idInscripcion` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `idInscripcion` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
