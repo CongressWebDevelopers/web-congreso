c00466835478c60bc9359f5ff9cb4387c9d8405f
ivanortegaalba <ivanortegaalba@gmail.com>
2015-06-20 01:23:15 +0200

    Corregidos algunos fallos

--- php/ajax/buscar_inscritos_ajax.php
+++ php/ajax/buscar_inscritos_ajax.php
@@ -1,27 +1,22 @@
 <?php
 
-include_once '../database/ORM.php';
-
+include_once 'php/model/containers/ContenedorInscripcion.php';
 $patron = $_REQUEST['patron'];
+
+$cInscripcion = new ContenedorInscripcion();
 $response = '';
+$lInscripcion = $cInscripcion->buscarInscripciones($patron);
 
-$consulta = "SELECT * FROM Inscripcion WHERE nombre LIKE '".$patron."%'";
-
-//$conexion = new ORM();
-//$resultado = $conexion->query($consulta);
-
-$conexion = mysql_connect('localhost', 'admin-congreso', 'congreso');
-$abreBD = mysql_select_db('db_CEIIE', $conexion);       
-$resultado = mysql_query($consulta, $conexion);
-$num_filas = mysql_num_rows($resultado);
-
-if ($num_filas>0){
+if ($lInscripcion){
 	$response .= '<center>';
-	$response .= '<table id="lista-inscritos" ALIGN="CENTER">';
+	$response .= '<table id="lista-inscritos" align="center">';
 	$response .= '<thead><tr><th>ID</th><th>Nombre</th><th>Teléfono</th></tr><thead>';
-		
-    while ($fila = mysql_fetch_array($resultado)){
-		$response .= '<tr><td align="center">'.$fila[0].'</td><td align="center"><a href="index.php?page=mi-inscripcion&idInscripcion='.$fila[0].'">'.$fila[1].'</a> </td><td align="center">'.$fila[3].'</td></tr>';
+        foreach ($lInscripcion as $i){
+		$response .= '<tr>'
+                        . '<td align="center">'.$i->getId().'</td>'
+                        . '<td align="center"><a href="index.php?page=mi-inscripcion&idInscripcion='.$i->getId().'">'.$i->getNombre().'</a> </td>'
+                        . '<td align="center">'.$i->getTelefono().'</td>'
+                        . '</tr>';
 	}
 	$response .= '</table>';
 	$response .= '</center>';
@@ -29,9 +24,4 @@ if ($num_filas>0){
 else{
 	$response .= '<p>Ninguna Coincidencia</p>';
 }
-
-//$conexion->close();
-
-mysql_close($conexion);
-
 echo $response;
